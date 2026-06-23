da774912212c26858a470ea863ee6aef979605a1
ibeneme <ikennaibenemee@gmail.com>
2026-06-23 14:03:55 +0100

    updates

--- app/blog/view/[slug]/page.tsx
+++ app/blog/view/[slug]/page.tsx
@@ -1,20 +1,13 @@
-import BlogPostEach from "./BlogPostEach";
 import { supabase } from "@/lib/configs/supabase";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // or remove if you want static
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
 
-  const { data: initialPost, error } = await supabase
-    .from("articles")
-    .select("*")
-    .eq("slug", slug)
-    .single();
-
-  if (error || !initialPost) {
-    return <div>Article not found</div>;
-  }
-
-  return <BlogPostEach slug={slug} initialPost={initialPost} />;
+  return <>{slug}</>;
 }
