f2222d487ee5273aca06aa888672489b1c31bbce
ibeneme <ikennaibenemee@gmail.com>
2026-06-23 14:31:40 +0100

    updates

--- lib/configs/supabase.ts
+++ lib/configs/supabase.ts
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://icstsbvoookvzyxhjwre.supabase.co'
+const supabaseKey = 'sb_publishable_9dc0h-8ClfWTdNrZo6Ec_Q_eAjthZ6r'
 
-// Throw a clear error if the environment variables are missing
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Missing Supabase environment variables!");
 }
 
-// Now TypeScript knows they are strings
 export const supabase = createClient(supabaseUrl, supabaseKey);
